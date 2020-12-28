a60941a72bd7b4556af978793b6bf43b8bd3d196
Valentin Vergara Hidd <valentinvergara@gmail.com>
2020-12-28 03:16:12 -0300

    first commit

---
+++ .doom.d/config.el
@@ -0,0 +1,105 @@
+(setq user-full-name "Valentin Vergara Hidd"
+      user-mail-address "valentinvergara@gmail.com")
+
+(delete-selection-mode 1)
+
+(if (eq system-type 'darwin)
+    (add-to-list 'initial-frame-alist '(fullscreen . maximized)))
+
+(setq fancy-splash-image "~/.doom.d/smithers.png")
+
+(setq doom-theme 'doom-peacock)
+(map! :leader
+      :desc "Load a new theme"
+      "h t" #'counsel-load-theme)
+
+(setq doom-font (font-spec :family "Hasklig" :size 23)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 23)
+      doom-big-font (font-spec :family "Hasklig" :size 35))
+
+(after! doom-themes
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t))
+
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic :weight bold))
+
+(setq org-latex-pdf-process '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+                              "bibtex %b"
+                              "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+                              "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+(after! org
+  (setq org-ellipsis " ▼ "
+        org-hide-emphasis-markers t
+        org-file-apps '((".pdf" . "zathura %s"))))
+
+(use-package! org-ref
+  :after org
+  :init
+  (setq org-ref-default-bibliography '("~/Dropbox/galactica.bib")))
+        ;org-ref-completion-library 'org-ref-ivy-cite))
+
+(use-package! org-ref-bibtex
+  :after org)
+
+(use-package! doi-utils
+  :after org)
+
+(use-package! org-ref-arxiv
+  :after org)
+
+(use-package! org-ref-isbn
+  :after org)
+
+(setq org-latex-default-packages-alist '(("utf8" "inputenc" t ("pdflatex"))
+                                         ("" "graphicx" t)
+                                         ("" "wrapfig" t)
+                                         ("" "rotating" t)
+                                         ("" "grffile" t)
+                                         ("normalem" "ulem" t)
+                                         ("" "amsmath" t)
+                                         ("" "amssymb" t)
+                                         ("" "capt-of" t)))
+
+(setq org-latex-packages-alist '(("dvipsnames" "xcolor")
+                                 ("colorlinks=true,breaklinks=true,citecolor=cyan,urlcolor=blue" "hyperref")
+                                 ("" "natbib")
+                                 ("" "float")
+                                 ("" "ragged2e")
+                                 ("" "tabularx")
+                                 ("" "subcaption")
+                                 ("" "mdframed")))
+
+(setq org-latex-listings 'listings
+      org-latex-listings-options
+      '(("frame" "single")
+        ("backgroundcolor" "\\color{define}")
+        ("commentstyle" "\\color{codegreen}")
+        ("keywordstyle" "\\color{magenta}")
+        ("stringstyle" "\\color{codepurple}")
+        ("basicstyle" "\\linespread{0.9}\\fontsize{9}{12}\\selectfont\\ttfamily")
+        ("breakatwhitespace" "false")
+        ("breaklines" "true")
+        ("captionpos" "b")
+        ("keepspaces" "true")
+        ("numbers" "left")
+        ("numberstyle" "\\tiny\\color{gray}")
+        ("numbersep" "5pt")
+        ("showspaces" "false")
+        ("showstringspaces" "false")
+        ("showtabs" "false")
+        ("tabsize" "4")))
+
+(setq org-pretty-entities t)
+
+;(use-package! elpy
+;  :init (elpy-enable))
+;(use-package! company
+;  :init (setq company-idle-delay 0.2))
+;(use-package! lsp-mode)
+;(use-package! lsp-ui
+;  :after lsp-mode)
+;(use-package! lsp-pyright
+;  :after lsp-mode)
---
+++ README.md
@@ -0,0 +1,45 @@
+# Testing git `bare` repositories
+
+Everything you will find in this repository comes from my local machine's home folder. It consolidates everything from my rarely used `bashrc` to my everyday config files (like Emacs stuff). I need to be able to store everything in one place, while also being able to easily clone everything and getting it ready for a new installation. Also, sensible information must be private.
+
+## Inspiration
+The general instructions to do this are in [this article](https://www.atlassian.com/git/tutorials/dotfiles). Of course, I changed paths and things that apply to my machine. The long-term goal is to be able to use these files for my main machine and for the laptop (MacBook Pro 2018 touchbar).
+
+## Procedure
+If you want to implement this, follow these steps
+
+### 1. Start a `bare` git repository
+This is a step you execute in your machine.
+
+``` sh
+git init --bare $HOME/.dots
+```
+
+Next, add this to your shell's rc
+
+``` sh
+alias dots='/usr/bin/git --git-dir $HOME/.dots/ --work-tree=$HOME'
+```
+
+The next step is critical, because it allows you to work with several untracked files without filling your terminal. 
+
+``` sh
+dots config --local status/showUntrackedFiles no
+```
+
+### 2. Setting up a `GitHub` repository
+In order to set this up, I have to create an empty github repository following the standard procedure. Then,
+
+``` sh
+dots remote add dots git@github.com:nitnelav27/dots.git
+```
+
+### 3. Adding files
+As an example, if I want to add my `i3` config file, localted in `~/.config/i3`
+
+``` sh
+dots add ~/.config/i3/config
+dots commit -m "Adding i3 config"
+dots push dots master
+```
+
